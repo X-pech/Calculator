9e576335fd913ef75aa2752c3ce39a72ea70ee5d
Xpech <lisgml@gmail.com>
2019-01-09 20:46:33 +0300

    Tests added

--- parserlib/src/test/java/study/itmo/xpech/parserlib/ExampleUnitTest.java
+++ parserlib/src/test/java/study/itmo/xpech/parserlib/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package study.itmo.xpech.parserlib;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import study.itmo.xpech.parserlib.exceptions.DivisionByZeroException;
+import study.itmo.xpech.parserlib.exceptions.ParsingException;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +16,38 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        Parser parser = new Parser();
+        Double d = 4.0;
+        try {
+            assertEquals(d.compareTo(parser.eval("2    +  2")), 0);
+        } catch (Exception e) {
+            System.err.println(e.getCause().toString());
+        }
+    }
+
+    @Test
+    public void DivisionByZero() {
+        Parser parser = new Parser();
+        boolean flag = true;
+        try {
+            parser.eval("2/0");
+            flag = false;
+        } catch (ParsingException pe) {
+
+        }
+        assertTrue(flag);
+    }
+
+    @Test
+    public void DivisionByZero2() {
+        Parser parser = new Parser();
+        boolean flag = true;
+        try {
+            parser.eval("2/0.0");
+            flag = false;
+        } catch (ParsingException pe) {
+
+        }
+        assertTrue(flag);
     }
 }
